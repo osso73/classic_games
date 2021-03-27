664a18365c30eaabe1f50cc437d7611fb4e65ef3
osso73 <osso@gmx.es>
2021-03-27 10:12:38 +0100

    Changed GameBoard to operate as a grid, as per iss. #45

--- docs/games/snake.md
+++ docs/games/snake.md
@@ -8,6 +8,8 @@ In here, the player controls a snake that goes on the screen. There is a piece o
 
 Turning the snake can be done by swiping on the screen. The snake can turn 90 degrees in any direction.
 
+The screen can be either portrait or landscape. If the screen changes, you should start a new game to take the dimensions into account.
+
 
 ## Main screen
 
@@ -15,14 +17,32 @@ This is the main screen of the game.
 
 ![screenshot](../img/snake_screen.jpg)
 
-For now it's very simple, just the snake and the food.
+It is divided in two areas:
+
+- _top row_: it shows a toolbar with some buttons that will allow you different functions. Here you can see as well the current score of the game.
+
+- _playing ground_: this is where the snake will run.
+
+
 
 
 ## Buttons in toolbar
 
-None yet. To be developed in further versions.
+
+The buttons give you access to the following functions:
+
+  - **Start**: to start a new game. It will reset the score, and remove the snake and food. It will create a new snake with the head only, and spawn a new food.
+
+  - **Size**: (not yet implemented). To change the size of the head and food. They can be larger or smaller. At each click it will switch to the next size. You can cycle through a number of pre-defined sizes.
+
+  - **Speed**: (not yet implemented). To change the speed of the snake. At each click it will switch to the next speed. You can cycle through a number of pre-defined speeds.
+
+  - **Exit**: exit the app.
+
 
 
 ## Credits
 
 I found the snake head in [here](https://www.iconfinder.com/icons/3015218/dangerous_animal_reptile_serpent_head_snake_face_viper_icon). And the images for food are from [Perfect Icons](http://www.perfect-icons.com/index.htm) website. These images cannot be used for commercial purposes, otherwise they are free for personal use.
+
+The sounds I found on [Free Sound](https://freesound.org/), and some of them I had found over Internet. I'm not aware of any copyright or limitation to use these.
--- source/snake/main.py
+++ source/snake/main.py
@@ -25,35 +25,47 @@ from kivy.properties import (
 
 import os
 import random
+import webbrowser
 from functools import partial
 from time import sleep
 
 
-SIZE = 100
-SPEED = 0.3
+SPEED = 0.5
 MINIMUM_SWIPE = 50
 IMAGES = os.path.join(os.path.dirname(__file__), 'images')  # path of images
 
 
 class MainScreen(BoxLayout):
-    pass
+    def help(self):
+        webbrowser.open('https://osso73.github.io/classic_games/games/snake/')    
 
 
-class GameBoard(FloatLayout):
-    
+class GameBoard(Widget):
     '''
     This is the board of the game, where all logic occurs.
     '''
-    
+
     score = NumericProperty(0)
+    size_pixels = NumericProperty(0)
+    size_grid = ListProperty(0)
     
+
     def __init__(self, *args, **kwargs):
         super(GameBoard, self).__init__(*args, **kwargs)
         self.snake_parts = []
         self.active = False
-        Clock.schedule_interval(self.update, SPEED)
         self.sounds = self.load_sounds()
-        # Clock.schedule_once(self.start_game)
+        self.size_grid = 10, 10
+        Clock.schedule_interval(self.update, SPEED)
+
+    def set_size(self, *args):
+        side = 0.90 * min(*self.parent.size)
+        self.size_pixels = int(side / 10)
+        w = int(self.parent.width / self.size_pixels)
+        h = int(self.parent.height / self.size_pixels)
+        self.size = (w*self.size_pixels, h*self.size_pixels)
+        self.grid = w, h
+        
 
     def start_game(self, *args):
         # reset parameters
@@ -61,6 +73,7 @@ class GameBoard(FloatLayout):
         self.move_x = 0
         self.move_y = 0
         self.score = 0
+        self.set_size()
 
         # remove previous snake & food
         instances = (SnakeHead, SnakePart, Food)
@@ -68,44 +81,38 @@ class GameBoard(FloatLayout):
         for p in parts:
             self.remove_widget(p)
 
-        w, h = self.size
-        print('window size:', w, h)
-        w_int = int(w / SIZE)
-        h_int = int(h / SIZE)
-        
         # create snake
-        p = [int(w_int/2)*SIZE, int(h_int/2)*SIZE]
-        head = SnakeHead(pos=p)
+        head = SnakeHead()
         self.add_widget(head)
-        self.snake_parts.append(head)
+        head.pos_nm = 5, 5
+        self.snake_parts.append(head)          
 
         # create food
-        self.food = Food(w, h)
-        self.food.spawn(self.snake_parts)
+        self.food = Food()
         self.add_widget(self.food)
-        
+        self.food.spawn(self.snake_parts)
+
         # activate game
         self.play('start')
         self.active = True
         self.change_direction('RIGHT')
 
-    
+
     def update(self, *args):
         if not self.active:
             return
-        
+
         # save position of parts
         old_positions = []
         for part in self.snake_parts:
-            old_positions.append(list(part.pos))
-        
-        
+            old_positions.append(part.pos_nm)
+
         # move head
         head = self.snake_parts[0]
-        head.x += self.move_x
-        head.y += self.move_y
+        head.n += self.move_x
+        head.m += self.move_y
         head.open_mouth(self.food)
-       
+
         # check collision
         if self.collision():
             self.game_over()
@@ -114,36 +121,36 @@ class GameBoard(FloatLayout):
         for i, part in enumerate(self.snake_parts):
             if i == 0:
                 continue
-            part.pos = old_positions[i-1]
-        
+            part.pos_nm = old_positions[i-1]
+
         # check if food is found
-        if head.pos == self.food.pos:        
+        if head.pos_nm == self.food.pos_nm:
             self.score += 1
             self.play('eat')
             new_part = SnakePart()
-            new_part.pos = old_positions[-1]
-            self.snake_parts.append(new_part)
             self.add_widget(new_part)
+            new_part.pos_nm = old_positions[-1]
+            self.snake_parts.append(new_part)
             self.food.spawn(self.snake_parts)
             head.mouth_open = False
-            
-        
+
+
     def collision(self):
         # collision with body
         head = self.snake_parts[0]
         for part in self.snake_parts[1:]:
-            if head.pos == part.pos:
+            if head.pos_nm == part.pos_nm:
                 return True
 
         # collision with borders
-        if (head.y + SIZE > self.height or head.y < 0 or 
-            head.x < 0 or head.x + SIZE > self.width):
-            
+        if (head.y < self.y or (head.y + head.height) > (self.y + self.height) or
+            head.x < self.x or (head.x + head.width) > (self.x + self.width)):
+
             return True
-        
+
         return False
-        
-                
+
+
     def game_over(self):
         self.active = False
         self.play('end_game')
@@ -151,25 +158,24 @@ class GameBoard(FloatLayout):
           content=PopupMsg(text='Sorry, snake crashed!'))
         p.open()
 
-    
+
     def change_direction(self, direct, *args):
-        # if position not multiple of SIZE, re-schedule the function
         head = self.snake_parts[0]
         if direct is 'LEFT' and self.move_x == 0:
-            self.move_x = -SIZE
+            self.move_x = -1
             self.move_y = 0
             head.direction = direct
         elif direct is 'RIGHT' and self.move_x == 0:
-            self.move_x = SIZE
+            self.move_x = 1
             self.move_y = 0
             head.direction = direct
         elif direct is 'UP' and self.move_y == 0:
             self.move_x = 0
-            self.move_y = SIZE
+            self.move_y = 1
             head.direction = direct
         elif direct is 'DOWN' and self.move_y == 0:
             self.move_x = 0
-            self.move_y = -SIZE
+            self.move_y = -1
             head.direction = direct
 
     def on_touch_down(self, touch):
@@ -184,7 +190,7 @@ class GameBoard(FloatLayout):
         '''
         self.swipe_x = touch.x
         self.swipe_y = touch.y
-    
+
     def on_touch_up(self, touch):
         '''
         When touch up, compare coordinates with swipe_x, swipe_y to find
@@ -215,34 +221,34 @@ class GameBoard(FloatLayout):
                 else:
                     direction = 'UP'
             self.change_direction(direction)
-            
+
             return direction
-    
+
     def load_sounds(self):
         '''
         Load all sounds of the game, and put them into the dictionary.
-        
+
         Returns
         -------
         sound : dict
             The dictionary of sounds that have been loaded
         '''
-        sound = dict()        
+        sound = dict()
         folder = os.path.join(os.path.dirname(__file__),'audio')
         for s in ['bye', 'eat', 'start', 'end_game']:
             sound[s] = SoundLoader.load(os.path.join(folder, f'{s}.ogg'))
-            
+
         return sound
-    
+
     def play(self, sound):
         '''
         Play a sound from the dictionary of sounds.
-        
+
         Parameters
         ----------
         sound : string
             Key of the dictionary corresponding to the sound to be played.
-        
+
         Raises
         ------
         Exception
@@ -255,82 +261,106 @@ class GameBoard(FloatLayout):
 
     def bye(self):
         '''
-        Play a sound before closing the app, and wait a delay so the sound 
+        Play a sound before closing the app, and wait a delay so the sound
         can be heard (Trump saying 'thank you very much')
         '''
         self.play('bye')
         sleep(1.5)
-
-       
-
-class Food(Widget):
-    image = StringProperty()
-    size = SIZE, SIZE
     
-    def __init__(self, w, h, *args, **kwargs):
-        super(Food, self).__init__(*args, **kwargs)
-        self.w = int(w / SIZE) - 1
-        self.h = int(h / SIZE) - 1
-        self.images = os.listdir(os.path.join(IMAGES, 'food'))
+
+class GridElement(Widget):
+    n = NumericProperty()
+    m = NumericProperty()
+    pos_nm = ListProperty()
+    grid = ListProperty()
+    
+    def __init__(self, *args, **kwargs):
+        super(GridElement, self).__init__(*args, **kwargs)
+    
+    
+    def set_position(self):
+        self.x = self.parent.x + self.n * self.width
+        self.y = self.parent.y + self.m * self.height
+    
+    def on_n(self, *args):
+        self.pos_nm = [self.n, self.m]
+    
+    def on_m(self, *args):
+        self.pos_nm = [self.n, self.m]
+    
+    def on_pos_nm(self, *args):
+        self.n, self.m = self.pos_nm
+        self.set_position()
         
     
+
+
+class Food(GridElement):
+    image = StringProperty()
+
+    def __init__(self, *args, **kwargs):
+        super(Food, self).__init__(*args, **kwargs)
+        self.images = os.listdir(os.path.join(IMAGES, 'food'))
+
+
     def spawn(self, snake):
         p = self._get_pos()
-        positions = [part.pos for part in snake]
+        positions = [ part.pos_nm for part in snake ]
         while p in positions:
             p = self._get_pos()
-        
-        self.pos = p
+
+        self.pos_nm = p
         self.image = os.path.join(IMAGES, 'food', random.choice(self.images))
-    
+
+
     def _get_pos(self):
-        return [random.randint(0, self.w) * SIZE,
-                random.randint(0, self.h) * SIZE]
- 
-
-class SnakePart(Widget):
-    size = SIZE, SIZE
+        return [random.choice(range(self.grid[0])),
+                random.choice(range(self.grid[1]))]
 
 
-class SnakeHead(Widget):
+class SnakePart(GridElement):
+    pass
+
+
+class SnakeHead(GridElement):
     image = StringProperty()
     direction = StringProperty()
     mouth_open = BooleanProperty(False)
-    size = SIZE, SIZE
-    
+
+
     def __init__(self, *args, **kwargs):
         super(SnakeHead, self).__init__(*args, **kwargs)
         self.head_images = dict()
         self.head_images['LEFT'] = dict()
         self.head_images['LEFT'][True] = os.path.join(IMAGES, 'head', 'open_left.png')
         self.head_images['LEFT'][False] = os.path.join(IMAGES, 'head', 'head_left.png')
-        
+
         self.head_images['RIGHT'] = dict()
         self.head_images['RIGHT'][True] = os.path.join(IMAGES, 'head', 'open_right.png')
         self.head_images['RIGHT'][False] = os.path.join(IMAGES, 'head', 'head_right.png')
-        
+
         self.head_images['UP'] = dict()
         self.head_images['UP'][True] = os.path.join(IMAGES, 'head', 'open_up.png')
         self.head_images['UP'][False] = os.path.join(IMAGES, 'head', 'head_up.png')
-        
+
         self.head_images['DOWN'] = dict()
         self.head_images['DOWN'][True] = os.path.join(IMAGES, 'head', 'open_down.png')
         self.head_images['DOWN'][False] = os.path.join(IMAGES, 'head', 'head_down.png')
 
-    
+
     def on_direction(self, *args):
         self.change_image()
-        
+
     def on_mouth_open(self, *args):
         self.change_image()
-        
+
     def open_mouth(self, food):
-        radius = SIZE
+        radius = self.width
         if abs(food.x - self.x) <= radius and abs(food.y - self.y) <= radius:
             self.mouth_open = True
         else:
             self.mouth_open = False
-    
+
     def change_image(self):
         self.image = self.head_images[self.direction][self.mouth_open]
 
@@ -338,6 +368,8 @@ class SnakeHead(Widget):
 class MenuButton(Button):
     pass
 
+class MenuButtonSmall(Button):
+    pass
 
 class MenuLabel(Label):
     pass
@@ -346,7 +378,7 @@ class PopupMsg(Label):
     pass
 
 
-class SnakeApp(App):    
+class SnakeApp(App):
     def build(self):
         self.icon = os.path.join(IMAGES, 'icon.png')
         main = MainScreen()
@@ -357,4 +389,3 @@ if __name__ == '__main__':
     # Window.size = (1080, 2340)
     # Window.size = (375, 800)
     SnakeApp().run()
-
